99e429b4b89166752c5b87bc029c9bed081f401d
Mohammad Ezmerli <mohammad.ezmerli@gmx.de>
2021-03-27 16:14:37 +0100

    Add project files.

---
+++ ScrumRandomizerApp/AppSettingsItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrumRandomizerApp
+{
+    public class AppSettingsItem
+    {
+        public int Id { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
---
+++ ScrumRandomizerApp/AppState.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace ScrumRandomizerApp
+{
+    public static class AppState
+    {
+        public static async Task<AppSettingsItem> GetAppSettings()
+        {
+            AppSettingsItem appSettingsItem = null;
+
+            var db = await AppDatabase.Instance;
+            appSettingsItem = await db.GetAppSettings();
+
+            return appSettingsItem 
+                ?? new AppSettingsItem();
+        }
+    }
+}
