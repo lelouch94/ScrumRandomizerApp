9e8e052959c848d5d6a2680270f72e191c8323b6
Mohammad Ezmerli <mohammad.ezmerli@gmx.de>
2021-03-27 17:07:41 +0100

    refactoring

---
+++ ScrumRandomizerApp/Persistancy/Models/ScrumParticipant.cs
@@ -0,0 +1,8 @@
+﻿namespace ScrumRandomizerApp.Persistancy.Models
+{
+    public class ScrumParticipant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
